20631e3c9b132eed4f5b84ec9c219c8aede40c18
Garret <gwiats@gmail.com>
2021-05-04 17:11:03 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,46 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({reviews}) => {
+    // return(
+    //     console.log('inisde MvRv')
+    // )
+    const lis = reviews.map(movie => 
+        <li className={'review'}>
+            <h3>{movie.display_title}</h3>
+            <p>{movie.summary_short}</p>
+            <a href={movie.link.url}>{movie.link.suggested_link_text}</a>
+            <br></br>
+            <br></br>
+        </li>
+    )
+    return(
+        <div className={'review-list'}>
+            {lis}
+        </div>
+    )
+}
+
+export default MovieReviews
+
+
+// byline: "Teo Bugbee"
+// critics_pick: 0
+// date_updated: "2020-09-10 11:04:05"
+// display_title: "Space Dogs"
+// headline: "‘Space Dogs’ Review: No Dogs Go to Heaven"
+// link:
+//      suggested_link_text: "Read the New York Times Review of Space Dogs"
+//      type: "article"
+//      url: "https://www.nytimes.com/2020/09/10/movies/space-dogs-review.html"
+//      __proto__: Object
+// mpaa_rating: ""
+// multimedia:
+//      height: 140
+//      src: "https://static01.nyt.com/images/2020/09/10/arts/10spacedogs-art2/10spacedogs-art2-mediumThreeByTwo210.jpg"
+//      type: "mediumThreeByTwo210"
+//      width: 210
+//      __proto__: Object
+// opening_date: "2020-09-11"
+// publication_date: "2020-09-10"
+// summary_short: "This documentary uses the lives and legends of Moscow’s street dogs to present a brutal vision of humanity and nature."
+// __proto__: Object
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,50 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=KKN0mtjHaco2GfQhoR3KIjGlFlSyW2ax&query='
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    
+    state = {
+        reviews: [],
+        searchTerm: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('test')
+        fetch(URL + this.state.searchTerm)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            this.setState({
+                reviews: data.results
+            })
+        })
+    }
+
+
+    render() {
+
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}> 
+                    <label>Search For Movie Reviews </label>
+                    <input value={this.state.searchTerm} onChange={this.handleChange}></input>
+                </form>
+                <ul>
+                <MovieReviews reviews={this.state.reviews} />
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default SearchableMovieReviewsContainer
